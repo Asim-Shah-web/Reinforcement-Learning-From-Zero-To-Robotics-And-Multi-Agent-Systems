f34f2c758a3238a11715d2e0718a6414cd23534b
Asim Shah <asimshahicp@gmail.com>
2026-09-08 14:46:54 +0500

    robotic arm wrapper code added

---
+++ code/envs/robotic_arm_wrapper.py
@@ -0,0 +1,15 @@
+"""Wrapper for a robotic manipulator environment for a later textbook example.
+
+The eventual implementation will wrap a MuJoCo or PyBullet manipulator task.
+"""
+
+
+from __future__ import annotations
+
+
+def make_env():
+    """Create the robotic arm environment.
+
+    This wrapper will be implemented when the textbook reaches Chapter 22.
+    """
+    raise NotImplementedError
